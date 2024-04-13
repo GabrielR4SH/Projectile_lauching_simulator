d82c448027d7fd32a48599840bc226124c5391e6
GabrielR4SH <bieelsantana23@hotmail.com>
2024-04-13 18:39:00 -0300

    Implementação de reações após o projetil cair

---
+++ Physucal_One/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physucal_One
+{
+    public class Person
+    {
+        public bool Head { get; set; }
+        public bool Shoulders { get; set; }
+        public bool Foot { get; set; }
+
+        public Person()
+        {
+            Head = true;
+            Shoulders = true;
+            Foot = true;
+        }
+    }
+}
--- Physucal_One/Program.cs
+++ Physucal_One/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Physucal_One;
+using System;
 
 class Program
 {
@@ -24,5 +25,40 @@ class Program
         Console.WriteLine();
         Console.WriteLine($"Tempo de voo: {timeOfFlight:F2} segundos");
         Console.WriteLine($"Distância percorrida: {distance:F2} metros");
+        
+
+        if (timeOfFlight >= 45)
+        {
+            Person person = new Person();
+
+            Random random = new Random();
+            int hitLocation = random.Next(1, 4); // 1 = cabeça, 2 = ombros, 3 = pé
+
+            switch (hitLocation)
+            {
+                case 1:
+                    person.Head = false;
+                    Console.WriteLine("A pessoa foi atingida na cabeça e não resistiu. 0 chance de vida");
+                    break;
+                case 2:
+                    person.Shoulders = random.Next(0, 2) == 1; // 50% de chance de sobreviver
+                    if (person.Shoulders)
+                        Console.WriteLine("A pessoa sobreviveu ao impacto nos ombros. ela só tinha 50% de chances de vida");
+                    else
+                        Console.WriteLine("A pessoa foi atingida nos ombros e não resistiu mesmo com 50% de chances de vida.");
+                    break;
+                case 3:
+                    person.Foot = random.Next(0, 6) < 5; // 80% de chance de sobreviver
+                    if (person.Foot)
+                        Console.WriteLine("A pessoa sobreviveu ao impacto nos pés. ela tinha 80% de chance de vida");
+                    else
+                        Console.WriteLine("A pessoa foi atingida nos pés e não resistiu mesmo com 80% de chances de vida.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ninguem foi atingido");
+        }
     }
 }
